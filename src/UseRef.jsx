998c1d857ca8f14271991e231db202bb8e026ad3
the-soban <sobanshafeq123@gmail.com>
2023-07-28 14:34:41 +0500

    initialized useRef hook file

---
+++ src/UseRef.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from "react";
+// import { useState } from "react";
+
+const UseRef = () => {
+
+    return(
+        <>
+            <h1>useEffect hook example:</h1>
+            <br />
+            <h2>Soban</h2>
+            <input type="text" placeholder="Enter your name..." />
+            <button>Change Name</button>
+        </>
+    );
+}
+
+export default UseRef;
